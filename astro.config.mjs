2931a50967ef3331e3d48f3a0256c07e9da1f07a
PiessensLander <55790048+PiessensLander@users.noreply.github.com>
2024-03-23 01:09:05 +0100

    Dark mode + Tally

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
-import sitemap from '@astrojs/sitemap';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://usecupper.com',
-	integrations: [sitemap()],
+  site: 'https://usecupper.com',
+  integrations: [tailwind({applyBaseStyles: false}), react()]
 });
